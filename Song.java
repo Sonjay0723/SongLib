9cabeb60b64504245eb99b01d96aa7d9ac7bd030
Jayson R Pitta <39282131+Sonjay0723@users.noreply.github.com>
2020-02-15 15:23:51 -0500

    Update Song.java

--- Song.java
+++ Song.java
@@ -5,9 +5,9 @@ public class Song {
 	private String name;
 	private String artist;
 	private String album;
-	private String year;
+	private int year;
 	
-	public Song(String name, String artist, String album, String year) {
+	public Song(String name, String artist, String album, int year) {
 		this.name = name;
 		this.artist = artist;
 		this.album = album;
@@ -26,7 +26,36 @@ public class Song {
 		return album;
 	}
 	
-	public String getYear() {
+	public int getYear() {
 		return year;
+	}
+	
+	public void setName(String x) {
+		this.name = x;
+	}
+	
+	public void setArtist(String x) {
+		this.artist = x;
+	}
+	
+	public void setAlbum(String x) {
+		this.album = x;
+	}
+	
+	public void setYear(int x) {
+		this.year = x;
+	}
+	
+	public String toString() {
+		return (this.name + " - " + this.artist);
+	}
+	
+	public int compareTo(Song x) {
+		int a = this.name.toLowerCase().compareTo(x.name.toLowerCase());
+		
+		if (a == 0)
+			return this.artist.toLowerCase().compareTo(x.artist.toLowerCase());
+		else
+			return a;
 	}
 }
