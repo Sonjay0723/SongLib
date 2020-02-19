211d74e0849fa807e4d5285e8426d6e78ef7474e
Jayson R Pitta <39282131+Sonjay0723@users.noreply.github.com>
2020-02-18 20:06:26 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,18 +3,31 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.*;
+import Display.SongController;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/Display/SongDisplay.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			
+			SongController libraryController = loader.getController();
+			libraryController.start(primaryStage);
+			
+			//AnchorPane root = (FXMLLoader.load(getClass().getResource("/SongLib/Display/SongDisplay.fxml")));
+			
+			Scene scene = new Scene(root,620,380);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Song Library");
+			primaryStage.setResizable(false);  
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
