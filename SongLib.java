ce1ef0703a25f52e91c9317cc6fc49c2fe4d8107
Jayson R Pitta <39282131+Sonjay0723@users.noreply.github.com>
2020-02-20 16:24:29 -0500

    Add files via upload

---
+++ SongLib.java
@@ -0,0 +1,36 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.*;
+import Display.SongController;
+
+
+public class SongLib extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/Display/SongDisplay.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			
+			SongController libraryController = loader.getController();
+			libraryController.start(primaryStage);
+			
+			Scene scene = new Scene(root,450,425);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Song List View");
+			primaryStage.setResizable(false);  
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[]) {
+		launch(args);
+	}
+}
